abb80c573b59c695d027d5ed3ba5b42b916944c9
R4YH4N1 <rayhan.mehmood@gmail.com>
2019-06-07 01:29:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Input the number to be multiplied");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+            Console.WriteLine("Input the number to multiply by");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            int z = x * y;
+
+            Console.WriteLine("Your answer is {0}", z);
+            Console.ReadLine();
+
+        }
+    }
+}
